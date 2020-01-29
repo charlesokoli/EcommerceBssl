326ff298a897546f221a7e97e0e31da1209ee7a2
Charles Okoli <charlesokoli63@gmail.com>
2020-01-29 10:52:11 +0100

    model update

--- EcommerceBssl/Models/Product.cs
+++ EcommerceBssl/Models/Product.cs
@@ -14,7 +14,7 @@ namespace EcommerceBssl.Models
         public Double OldPrice { get; set; }
         public int SubCategoryId { get; set; }
         public SubCategory SubCategory { get; set; }//property of the class product
-        public ICollection<ProductColour> ProductColour { get; set; }
-        public ICollection<Image> Images { get; set; }
+        public IList<ProductColour> ProductColour { get; set; }
+        public IList<Image> Images { get; set; }
     }
 }
